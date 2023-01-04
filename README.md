5a3e5ba9a4b90b94f54e05593e9ad9a48e8f71d8
hellopsm <psm2000163@163.com>
2023-01-04 19:51:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-quanping-jubuquanping
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-p1qgga)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <lay-fullscreen :target="elRef" v-slot="{ enter, exit, toggle, isFullscreen }" @fullscreenchange=fullscreen2>
+    <div ref="elRef" class="wrapper-fullscreen">
+      <lay-button type="normal" @click="enter()">进入全屏</lay-button>  
+      <lay-button type="normal" @click="exit()">退出</lay-button> 
+      <lay-button type="warm" @click="toggle()"> 切换: {{isFullscreen ? "退出" : "进入全屏"}} </lay-button>
+    </div>
+  </lay-fullscreen>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const elRef = ref(null);
+    const fullscreen2 = function(isFullscreen){
+      console.log(isFullscreen)
+    }
+    return {
+      elRef,
+      fullscreen2
+    }
+  }
+}
+</script>
+<style>
+.wrapper-fullscreen{
+  padding:10px;
+  border:1px solid #dddddd;
+  background-color:#F6F6F6;
+  display:inline-block
+}
+</style>
